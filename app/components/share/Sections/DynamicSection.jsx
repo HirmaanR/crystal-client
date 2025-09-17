95d942740336f271f1eb6861e74e696abf455dbd
HirmaanR <rashidabadi.8511@gmail.com>
2025-09-17 11:59:25 +0330

    style: 💄 improve title style in dynamicSection

--- app/components/share/Sections/DynamicSection.jsx
+++ app/components/share/Sections/DynamicSection.jsx
@@ -3,7 +3,7 @@ function DynamicSection({ title, children }) {
   return (
     <div className="grid grid-col-1 py-5 justify-center text-center">
       <div className="max-w-lg md:max-w-5xl">
-        <h1 className="font-bold text-4xl">{title}</h1>
+        <h1 className="font-bold text-3xl md:text-4xl">{title}</h1>
       </div>
       <div className="flex justify-center items-center w-full my-5">
         {children}
