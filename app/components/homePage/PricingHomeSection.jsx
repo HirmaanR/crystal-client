1bcbec22d399891629bd3464ba0c5022f8f8497a
HirmaanR <rashidabadi.8511@gmail.com>
2025-09-18 17:19:31 +0330

    style: 💄 implmentation recommended card style

--- app/components/homePage/PricingHomeSection.jsx
+++ app/components/homePage/PricingHomeSection.jsx
@@ -3,9 +3,9 @@ import DynamicSection from "../share/Sections/DynamicSection"
 
 function PriceCard({ recommended = false }) {
   return (
-    <div className={`card w-full rounded-xl h-full ${recommended ? "bg-neutral text-base-200" : "bg-base-200"} shadow-sm`}>
+    <div className={`card card-xl w-full rounded-xl h-full capitalize ${recommended ? "bg-neutral text-base-200" : "bg-base-200"} shadow-sm`}>
       <div className="card-body">
-        <span className="badge badge-xs badge-warning">Most Popular</span>
+        <span className="badge badge-xs badge-warning">{recommended ? "Recommended" : "Most Popular"}</span>
         <div className="flex justify-between">
           <h2 className="text-3xl font-bold">Premium</h2>
           <span className="text-xl">$29/mo</span>
@@ -22,6 +22,18 @@ function PriceCard({ recommended = false }) {
           <li>
             <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
             <span>Batch processing capabilities</span>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+            <span>AI-driven image enhancements</span>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+            <span>AI-driven image enhancements</span>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+            <span>AI-driven image enhancements</span>
           </li>
           <li>
             <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
