cf254bc973e917c64ba124904b4857eccaa8d5fd
HirmaanR <rashidabadi.8511@gmail.com>
2025-09-21 18:04:52 +0330

    feat: 🎸 make dynamic props for sub cards

--- app/components/homePage/PricingHomeSection.jsx
+++ app/components/homePage/PricingHomeSection.jsx
@@ -1,52 +1,22 @@
 import DynamicSection from "../share/Sections/DynamicSection"
 
 
-function PriceCard({ recommended = false }) {
+function PriceCard({ recommended = false, badage = "Most Popular", title = "Premium", amount = "29/mo", options = ["High-resolution image generation", "High-resolution image generation", "High-resolution image generation", "High-resolution image generation", "High-resolution image generation"] }) {
   return (
     <div className={`card card-xl w-full rounded-xl h-full capitalize ${recommended ? "bg-neutral text-base-200" : "bg-base-200"} shadow-sm`}>
       <div className="card-body">
-        <span className="badge badge-xs badge-warning">{recommended ? "Recommended" : "Most Popular"}</span>
+        <span className="badge badge-xs badge-warning">{recommended ? "Recommended" : badage}</span>
         <div className="flex justify-between">
-          <h2 className="text-3xl font-bold">Premium</h2>
-          <span className="text-xl">$29/mo</span>
+          <h2 className="text-3xl font-bold">{title}</h2>
+          <span className="text-xl">${amount}</span>
         </div>
         <ul className="mt-6 flex flex-col gap-2 text-xs">
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>High-resolution image generation</span>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>Customizable style templates</span>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>Batch processing capabilities</span>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>AI-driven image enhancements</span>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>AI-driven image enhancements</span>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>AI-driven image enhancements</span>
-          </li>
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span>AI-driven image enhancements</span>
-          </li>
-          <li className="opacity-50">
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span className="line-through">Seamless cloud integration</span>
-          </li>
-          <li className="opacity-50">
-            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-            <span className="line-through">Real-time collaboration tools</span>
-          </li>
+          {options.map((item) => {
+            return <li>
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span>{item}</span>
+            </li>
+          })}
         </ul>
         <div className="mt-6">
           <button className="btn btn-primary btn-block rounded-3xl">Subscribe</button>
