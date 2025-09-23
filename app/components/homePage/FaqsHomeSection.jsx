a87c8fa247162d779f741dd13886bb6b244e3cec
HirmaanR <rashidabadi.8511@gmail.com>
2025-09-23 21:49:29 +0330

    feat: 🎸 improve faqs section
    change title text - better responsive - improve faqs text style

--- app/components/homePage/FaqsHomeSection.jsx
+++ app/components/homePage/FaqsHomeSection.jsx
@@ -9,18 +9,18 @@ function FaqsHomeSection({ }) {
   ]
 
   return (
-    <div className="grid grid-cols-1 bg-orange-500 md:grid-cols-2 text-left gap-5 px-20 mb-32">
-      <div className="bg-blue-500">
-        <h1 className="font-bold text-2xl">title</h1>
-        <p className="font-light text-lg">description</p>
+    <div className="grid grid-cols-1 lg:grid-cols-2 text-left gap-5 px-20 mb-32 capitalize">
+      <div className="capitalize gap-5 flex flex-col">
+        <h1 className="font-bold text-4xl">Frequently <span className="text-primary">asked questions</span></h1>
+        <p className="font-light text-lg">For any unanswered questions, reach out to our support team via contact us page or email. We'll respond with-in a day to assist you.</p>
       </div>
       <div className="grid grid-cols-1 gap-3">
         {faqs.map((item, index) => {
           return (
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <div key={index} className="collapse bg-base-100 border border-base-300">
               <input type="radio" name="my-accordion-3" defaultChecked={index === 0} />
-              <div className="collapse-title font-semibold">{item.title}</div>
-              <div className="collapse-content text-sm">{item.content}</div>
+              <div className="collapse-title font-semibold text-lg">{item.title}</div>
+              <div className="collapse-content text-sm font-medium">{item.content}</div>
             </div>
           )
         })}
